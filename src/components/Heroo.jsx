ec957bc7793c808d7bdc83de538784d039d92238
fadoua bach <fadouaba.97@gmail.com>
2026-01-06 20:25:24 +0100

    navbar

---
+++ src/components/Heroo.jsx
@@ -0,0 +1,9 @@
+function Hero() {
+  return (
+    <section className="hero">
+      <h1>Things to do in Agadir</h1>
+    </section>
+  )
+}
+
+export default Hero
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './components/Nav.css'
+import './components/Hero.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
