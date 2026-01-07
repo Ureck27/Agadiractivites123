46579743ae063a1b9dd5cf2e53f5cff7c241e2f1
rachid aourik <rachidaourik18@gmail.com>
2026-01-07 10:50:37 +0100

    a

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-=======
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Carde from "./carde.jsx";
->>>>>>> lahosain
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
