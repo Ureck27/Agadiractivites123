fd16eb589a855f0df526cdc006e59822feaa946c
rachid aourik <rachidaourik18@gmail.com>
2026-01-07 11:24:00 +0100

    fix merging bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import CardsSection from './assets/components/CardsSection'
-
+import carde from './Carde'
 function App() {
 
   return (
     <>
+     <Carde />
      <CardsSection />
     </>
   )
